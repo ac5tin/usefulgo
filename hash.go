08819440cc8970e2b09e5814e66fc56113788dc7
ac5tin <austinchang4@gmail.com>
2020-04-09 13:08:10 +0800

    more hashing maps

--- hash.go
+++ hash.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -14,4 +18,23 @@ func HashPassword(password string, cost int) (string, error) {
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+// Mapfields - returns subset map
+func Mapfields(d map[string]interface{}, fields []string) map[string]interface{} {
+	retme := make(map[string]interface{})
+	for _, field := range fields {
+		retme[field] = d[field]
+	}
+	return retme
+}
+
+// Hashmap hashes a map using sha256 and returns the hash as string
+func Hashmap(d map[string]interface{}) (string, error) {
+	b, err := json.Marshal(d)
+	if err != nil {
+		return "", err
+	}
+	h := sha256.Sum256(b)
+	return fmt.Sprintf("%x", h), nil
 }
