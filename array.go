e881bd0fdd1e943febf2fd17b838b85cabc5229f
austin <austinchang4@gmail.com>
2022-03-20 02:47:53 +0800

    array utils use generics

--- array.go
+++ array.go
@@ -1,17 +1,18 @@
 package utils
 
 // ArrContains check is array contains a string
-func ArrContains(arr []string, str string) bool {
+
+func ArrContains[T Primitive](arr []T, tgt T) bool {
 	for _, a := range arr {
-		if a == str {
+		if a == tgt {
 			return true
 		}
 	}
 	return false
 }
 
-// ArrRMS removes string element from array (ordered)
-func ArrRMS(slice *[]string, t string) {
+// ArrRM removes element from array (ordered)
+func ArrRm[T Primitive](slice *[]T, t T) {
 	for i, a := range *slice {
 		if a == t {
 			*slice = append((*slice)[:i], (*slice)[i+1:]...)
@@ -21,74 +22,16 @@ func ArrRMS(slice *[]string, t string) {
 	return
 }
 
-// ArrRmiF - fast array remover by index struct (order unmaintained)
-type ArrRmiF struct{}
-
-// ArrRmiS - Slow array remover by index struct (order maintained)
-type ArrRmiS struct{}
-
-// NewArrRmiF returns new ArrRmiF (fast array index remover)
-func NewArrRmiF() ArrRmiF {
-	return ArrRmiF{}
-}
-
-// NewArrRmiS returns new ArrRmiS (slow array index remover)
-func NewArrRmiS() ArrRmiS {
-	return ArrRmiS{}
-}
-
-// String - []string remover
-func (a ArrRmiF) String(slice *[]string, index uint32) {
+// ArrRmF removes element from array (unordered,fast)
+func ArrRmF[T any](slice *[]T, index uint32) {
 	(*slice)[index] = (*slice)[len(*slice)-1]
-	(*slice)[len(*slice)-1] = ""
+	(*slice)[len(*slice)-1] = *new(T)
 	(*slice) = (*slice)[:len(*slice)-1]
 }
 
-// Object - []map[string]interface{} remover
-func (a ArrRmiF) Object(slice *[]map[string]interface{}, index uint32) {
-	(*slice)[index] = (*slice)[len(*slice)-1]
-	(*slice)[len(*slice)-1] = map[string]interface{}{}
-	(*slice) = (*slice)[:len(*slice)-1]
-}
-
-// Int - []int remover
-func (a ArrRmiF) Int(slice *[]int, index uint32) {
-	(*slice)[index] = (*slice)[len(*slice)-1]
-	(*slice)[len(*slice)-1] = 0
-	(*slice) = (*slice)[:len(*slice)-1]
-}
-
-// Any - []interface{} remover
-func (a ArrRmiF) Any(slice *[]interface{}, index uint32) {
-	(*slice)[index] = (*slice)[len(*slice)-1]
-	(*slice)[len(*slice)-1] = 0
-	(*slice) = (*slice)[:len(*slice)-1]
-}
-
-// Object - []map[string]interface{} remover
-func (a ArrRmiS) Object(slice *[]map[string]interface{}, index uint32) {
+// ArrRmS removes element from array (ordered,slow)
+func ArrRmS[T any](slice *[]T, index uint32) {
 	copy((*slice)[index:], (*slice)[index+1:])
-	(*slice)[len(*slice)-1] = map[string]interface{}{}
-	(*slice) = (*slice)[:len(*slice)-1]
-}
-
-// String - []string remover
-func (a ArrRmiS) String(slice *[]string, index uint32) {
-	copy((*slice)[index:], (*slice)[index+1:])
-	(*slice)[len(*slice)-1] = ""
-	(*slice) = (*slice)[:len(*slice)-1]
-}
-
-// Int - []int remover
-func (a ArrRmiS) Int(slice *[]int, index uint32) {
-	copy((*slice)[index:], (*slice)[index+1:])
-	(*slice)[len(*slice)-1] = 0
-	(*slice) = (*slice)[:len(*slice)-1]
-}
-
-// Any - []interface{} remover
-func (a ArrRmiS) Any(slice *[]interface{}, index uint32) {
-	copy((*slice)[index:], (*slice)[index+1:])
-	(*slice)[len(*slice)-1] = 0
+	(*slice)[len(*slice)-1] = *new(T)
 	(*slice) = (*slice)[:len(*slice)-1]
 }
